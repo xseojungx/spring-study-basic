14956bf9b2444b0776ed2114226adb5c8e682c0f
Seojung <sjhan0814@gmail.com>
2024-03-14 01:08:01 +0900

    3/13 spirng study

--- core/src/test/java/hello/core/order/OrderServiceTest.java
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -24,7 +24,7 @@ public class OrderServiceTest {
     }
 
     @Test
-    void createOrder(){
+    void createOrde(){
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
