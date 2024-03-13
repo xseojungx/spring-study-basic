855c5f8abc1e2e50752dfd79438a97f5d2e3ad39
Seojung <sjhan0814@gmail.com>
2024-03-14 01:07:44 +0900

    3/13 spirng study

--- core/src/test/java/hello/core/order/OrderServiceTest.java
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -22,4 +22,15 @@ public class OrderServiceTest {
 
         Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
     }
+
+    @Test
+    void createOrder(){
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
 }
